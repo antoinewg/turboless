b3ae0a036cc0d2519637334939bc36a0ca3f6df0
Antoine Garcia <a.garcia.walecha@gmail.com>
2022-04-30 19:00:26 +0200

    simplify package config

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,12 +1,8 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  extends: ["prettier"],
   settings: {
     next: {
-      rootDir: ["apps/*/", "packages/*/"],
+      rootDir: ["packages/*/", "services/*/"],
     },
-  },
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
   },
 };
