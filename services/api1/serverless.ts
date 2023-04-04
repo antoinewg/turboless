7e744f6ad5f4b48d8643b0defd2963bb36396483
Antoine Garcia <a.garcia.walecha@gmail.com>
2023-04-04 18:07:23 +0200

    use graviton2 architecture

--- services/api1/serverless.ts
+++ services/api1/serverless.ts
@@ -15,7 +15,8 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
-    deploymentMethod: 'direct'
+    deploymentMethod: 'direct',
+    architecture: 'arm64'
   },
   functions,
   package: { individually: true },
