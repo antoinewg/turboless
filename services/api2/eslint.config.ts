ef378a46ac56f244f9b36ec49b7726b4bd33e0d7
antoinewg <a.garcia.walecha@gmail.com>
2025-02-18 16:54:20 -0800

    run formatter

--- services/api2/eslint.config.ts
+++ services/api2/eslint.config.ts
@@ -1,4 +1,4 @@
-import config from 'eslint-config-custom/eslint.config.mjs';
- 
+import config from "eslint-config-custom/eslint.config.mjs";
+
 /** @type {import("eslint").Linter.Config} */
 export default config;
